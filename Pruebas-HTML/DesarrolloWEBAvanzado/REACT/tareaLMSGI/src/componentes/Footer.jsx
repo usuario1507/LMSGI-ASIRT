3076cf03026cdd2894c6369777066f77f78ca526
usuario1507 <agarlop1507@g.educaand.es>
2023-05-25 12:50:04 +0200

    Cambios10

--- Pruebas-HTML/DesarrolloWEBAvanzado/REACT/tareaLMSGI/src/componentes/Footer.jsx
+++ Pruebas-HTML/DesarrolloWEBAvanzado/REACT/tareaLMSGI/src/componentes/Footer.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import './footer.css'
+import {BsFacebook, BsTwitter,BsInstagram} from "react-icons/bs";
 
-const Footer = () => {
+export const Footer = () => {
   return (
-    <div>
- 
-    </div>
+    <footer>
+      <div>
+        <a href="https://www.facebook.com/"><BsFacebook/></a>
+        <a href="https://twitter.com/?lang=gl"><BsTwitter/></a>
+        <a href="https://www.instagram.com/"><BsInstagram/></a>
+      </div>
+    </footer>
   )
 }
 
